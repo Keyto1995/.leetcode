a2e2d2df7876f3f8158a2c4a43486ee095985c50
keyto1995 <keyto1995@outlook.com>
2019-10-10 13:26:34 +0800

    init project

---
+++ 1.两数之和.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (46.57%)
+ * Likes:    6216
+ * Dislikes: 0
+ * Total Accepted:    542.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ *
+ */
+func twoSum(nums []int, target int) []int {
+	nmap := make(map[int]int)
+	result := make([]int, 2)
+	for index, value := range nums {
+		complement := target - value
+		ti, ok := nmap[complement]
+		if ok {
+			result[1] = index
+			result[0] = ti
+			break
+		}
+		nmap[value] = index
+	}
+	return result
+}
---
+++ 136.只出现一次的数字.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=golang
+ *
+ * [136] 只出现一次的数字
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (63.23%)
+ * Likes:    871
+ * Dislikes: 0
+ * Total Accepted:    108.3K
+ * Total Submissions: 170K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ */
+
+// @lc code=start
+func singleNumber(nums []int) int {
+    result := 0
+    for _, value := range nums {
+        result ^= value
+    }
+    return result
+}
+// @lc code=end
+

8593ea80ae8f9fbbb5b1010f995ec614203da49b
keyto1995 <keyto1995@outlook.com>
2019-10-29 15:43:30 +0800

    7.整数反转

---
+++ 7.整数反转.go
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=golang
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (33.01%)
+ * Likes:    1301
+ * Dislikes: 0
+ * Total Accepted:    178.7K
+ * Total Submissions: 541.5K
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *
+ * 示例 1:
+ *
+ * 输入: 123
+ * 输出: 321
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: -123
+ * 输出: -321
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: 120
+ * 输出: 21
+ *
+ *
+ * 注意:
+ *
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
+ * 0。
+ *
+ */
+package main
+
+import "math"
+
+// @lc code=start
+func reverse(x int) int {
+	result := 0
+	for x != 0 {
+		result = result*10 + x%10
+		if result < math.MinInt32 || result > math.MaxInt32 {
+			return 0
+		}
+		x /= 10
+	}
+
+	return result
+}
+
+// @lc code=end
